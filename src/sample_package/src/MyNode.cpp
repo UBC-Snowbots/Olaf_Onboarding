f0d4c8a7e96064482b139759f8076ce9a986eb38
Marinah Zhao <marinahzhao@gmail.com>
2018-02-17 13:08:31 -0800

    revert

--- src/sample_package/src/MyNode.cpp
+++ src/sample_package/src/MyNode.cpp
@@ -23,7 +23,6 @@ MyClass::MyClass(int argc, char **argv, std::string node_name) {
     std::string topic_to_subscribe_to = "subscribe_topic";
     int refresh_rate = 10;
     my_subscriber = nh.subscribe(topic_to_subscribe_to, refresh_rate, &MyClass::subscriberCallBack, this);
-    
     // Setup Publisher(s)
     std::string topic = private_nh.resolveName("publish_topic");
     uint32_t queue_size = 1;
